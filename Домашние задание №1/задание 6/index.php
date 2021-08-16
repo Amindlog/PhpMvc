a86e8187397cf5a4b8472b8cfaf339fbeb4bb926
Александр Пономарев <mr.amind@yandex.ru>
2021-08-16 16:09:48 +0300

    1

--- Домашние задание №1/задание 6/index.php
+++ Домашние задание №1/задание 6/index.php
@@ -2,7 +2,7 @@
    <?php 
        for ($i = 0; $i <= 10 ; $i++) 
        { 
-           echo "\t<tr>";
+           echo "<tr>";
             for ($j = 0; $j <= 10; $j++) 
             { 
                 if ($i != 0 && $j != 0) {
@@ -12,4 +12,4 @@
             echo "<tr>";
        }
     ?>
-    </table>
+</table>
