d75ac12b959af58ca4ec39d5c95fb6843503b00b
Александр Пономарев <mr.amind@yandex.ru>
2021-08-16 15:40:55 +0300

    Добавление домашек

---
+++ Домашние задание №1/Задание 2/index.php
@@ -0,0 +1,12 @@
+<?php
+    define("VSEGO", 80);
+
+    define("FLOMASTER", 23);
+
+    define("KARANDASH", 40);
+
+    $kraski = '';
+
+    $kraski = VSEGO - (FLOMASTER + KARANDASH);
+
+    echo "Выполнено работ красками: ". $kraski;
---
+++ Домашние задание №1/Задание 4/index.php
@@ -0,0 +1,14 @@
+<?php
+    $day = random_int(1,9);
+
+    switch ($day) {
+        case $day <= 5:
+            echo "Это рабочий день";
+            break;
+        case $day >5 && $day <= 7:
+            echo 'Это выходной день';
+            break;
+        default:
+            echo "Неизвестный день";
+            break;
+    }
